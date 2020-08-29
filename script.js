afd1ea84e902f49b1e4f0885780c65f332d665bc
Jonathan Tran <43768831+jontran1@users.noreply.github.com>
2020-08-29 18:44:42 -0400

    Add alert messages warning the user.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 let n = 3;
 const gameManager = new GameManager(n);
 document.getElementById("depth").innerHTML = "Current Depth:" + gameManager.depth;
+alert("WARNING! Depending on the size of the board, depth, and algorthim, Your browser may crash. The number of games possible is exponential as n grows larger.")
+alert("Depth only applies to Minimax and Minimax Pruning.")
 gameManager.startGame();
 
 document.getElementById("resetButton").addEventListener("click", () => {
