20950af7f86d64ae3b2275b2bd373129cae7c2ab
Jonathan Tran <43768831+jontran1@users.noreply.github.com>
2020-08-29 17:58:05 -0400

    Allow the user to change size of the grid.

--- script.js
+++ script.js
@@ -7,10 +7,18 @@ gameManager.startGame();
 document.getElementById("resetButton").addEventListener("click", () => {
     gameManager.resetGame(n);
     gameManager.ai.depth = parseInt(depth);
+    document.getElementById("depth").innerHTML = "Current Depth:" + depth;
 });
 
 document.getElementById("setDepthButton").addEventListener("click", () => {
     depth = prompt("Enter a integer:");
     gameManager.depth = parseInt(depth);
     document.getElementById("depth").innerHTML = "Current Depth:" + depth;
+});
+
+document.getElementById("changeBoardSizeButton").addEventListener("click", () => {
+    newSize = prompt("Enter a integer:");
+    n = parseInt(newSize);
+    gameManager.resetGame(n);
+    document.getElementById("depth").innerHTML = "Current Depth:" + gameManager.depth;
 });
