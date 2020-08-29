765a4ba36325f052e8401ff3befee79dc3de28b7
Jonathan Tran <43768831+jontran1@users.noreply.github.com>
2020-08-29 17:54:15 -0400

    Give the user more control.
    Allows the user to reset game and change max depth for minimax and minimax pruning.

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 
-
-let gameManager = new GameManager(3);
+let n = 3;
+const gameManager = new GameManager(n);
+document.getElementById("depth").innerHTML = "Current Depth:" + gameManager.depth;
 gameManager.startGame();
+
+document.getElementById("resetButton").addEventListener("click", () => {
+    gameManager.resetGame(n);
+    gameManager.ai.depth = parseInt(depth);
+});
+
+document.getElementById("setDepthButton").addEventListener("click", () => {
+    depth = prompt("Enter a integer:");
+    gameManager.depth = parseInt(depth);
+    document.getElementById("depth").innerHTML = "Current Depth:" + depth;
+});
